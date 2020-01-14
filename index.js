fd5b30097cf5e92f4577bfcae4fcb41d97396f39
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-01-14 23:15:12 +0100

    Added rules for Node.js & use renamed rules for `package.json` file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const {
+  config: {recommended: nodeJsConfig},
+  rules: nodeJsRules
+} = require('@projectlint/plugin-node.js')
+const {
   config: {recommended: packageJsonConfig},
   rules: packageJsonRules
-} = require('@projectlint/rules-package.json')
+} = require('@projectlint/plugin-package.json')
 
 
 const configs =
 {
+  '@projectlint/plugin-node.js'    : nodeJsConfig,
   '@projectlint/rules-package.json': packageJsonConfig
 }
 
 const rules =
 {
+  '@projectlint/plugin-node.js'    : nodeJsRules,
   '@projectlint/rules-package.json': packageJsonRules
 }
 
